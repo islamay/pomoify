03cd79218abd79b04f90a8889a3c8013dac5b935
islamay <islamaydean@gmail.com>
2025-02-12 10:44:59 +0800

    Created pomodoro timer ui. solves #12

--- src/App.tsx
+++ src/App.tsx
@@ -1,10 +1,12 @@
 import React from "react";
 import { Navbar } from "./components/navbar";
+import { Pomodoro } from "./components/pomodoro";
 
 function App() {
     return (
         <React.Fragment>
             <Navbar />
+            <Pomodoro />
         </React.Fragment>
     );
 }
---
+++ src/components/pomodoro.tsx
@@ -0,0 +1,99 @@
+import { RotateCcw, SkipForward } from "lucide-react";
+import { Button } from "./button";
+import { useState } from "react";
+import clsx from "clsx";
+
+type Phase = "focus" | "break" | "long-break";
+
+type PhaseProps = {
+    children: React.ReactNode;
+    isActive?: boolean;
+    onClick?: () => void;
+};
+
+function Phase({ children, isActive, onClick }: PhaseProps) {
+    return (
+        <button
+            onClick={onClick}
+            className={clsx(
+                "h-8 content-center rounded cursor-pointer transition-colors",
+                {
+                    "bg-black font-semibold ": isActive,
+                    "text-secondary-foreground hover:bg-current/10": !isActive,
+                }
+            )}
+        >
+            {children}
+        </button>
+    );
+}
+
+interface PhasesProps extends React.HTMLProps<HTMLDivElement> {}
+
+function Phases({ className, ...props }: PhasesProps) {
+    const [phase, setPhase] = useState<Phase>("focus");
+
+    return (
+        <div
+            {...props}
+            className={`grid grid-cols-3 gap-1 px-1 py-1 bg-secondary rounded ${className}`}
+        >
+            <Phase
+                isActive={phase === "focus"}
+                onClick={() => setPhase("focus")}
+            >
+                Focus
+            </Phase>
+            <Phase
+                isActive={phase === "break"}
+                onClick={() => setPhase("break")}
+            >
+                Break
+            </Phase>
+            <Phase
+                isActive={phase === "long-break"}
+                onClick={() => setPhase("long-break")}
+            >
+                Long Break
+            </Phase>
+        </div>
+    );
+}
+
+function Pomodoro() {
+    return (
+        <section
+            aria-label="Pomodoro timer section"
+            className="max-w-lg md:px-0 mx-auto mt-8 px-8"
+        >
+            <div className="flex justify-between items-center">
+                <h3 className="font-semibold">Phase</h3>
+            </div>
+
+            <Phases className="mt-2" />
+
+            <h1 className="mt-8 text-[4rem] text-center font-mono font-medium">
+                25:00
+            </h1>
+
+            <div
+                aria-label="Timer control"
+                className="mt-8 flex items-center justify-center gap-4"
+            >
+                <Button variant="ghost" size="icon">
+                    <RotateCcw size={16} strokeWidth={1} />
+                </Button>
+                <Button size="large">Start</Button>
+                <Button variant="ghost" size="icon">
+                    <SkipForward size={16} strokeWidth={1} />
+                </Button>
+            </div>
+
+            <p className="mt-4 text-center text-sm text-secondary-foreground">
+                You have focused 0 time
+            </p>
+        </section>
+    );
+}
+
+export { Pomodoro };
