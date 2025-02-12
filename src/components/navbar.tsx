50b1f2b820a0085449ec19cb4e52dce4c42114fc
islamay <islamaydean@gmail.com>
2025-02-12 11:15:37 +0800

    fix responsiveness issue in pomodoro section

--- src/components/navbar.tsx
+++ src/components/navbar.tsx
@@ -2,7 +2,7 @@ import logo from "@/assets/logo.svg";
 
 function Navbar() {
     return (
-        <nav className="flex justify-between border-b py-4 max-w-xl px-8 min-[36rem]:px-0 min-[36rem]:mx-auto sm">
+        <nav className="flex justify-between border-b py-4 max-w-xl px-8 min-[36rem]:px-0 min-[36rem]:mx-auto">
             <a href="/">
                 <img src={logo} />
             </a>
