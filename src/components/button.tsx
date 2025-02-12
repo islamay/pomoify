fd5f9d484c6f3cf0b513f68b75bf31cd21cd4655
islamay <islamaydean@gmail.com>
2025-02-12 09:37:20 +0800

    Created button component. closes #9

---
+++ src/components/button.tsx
@@ -0,0 +1,41 @@
+import { cn } from "@/lib/utils";
+import { Slot } from "@radix-ui/react-slot";
+import { cva, VariantProps } from "class-variance-authority";
+
+const buttonVariants = cva(
+    "flex items-center rounded cursor-pointer transition-colors",
+    {
+        variants: {
+            variant: {
+                default:
+                    "font-semibold bg-primary text-primary-foreground hover:bg-primary/90",
+                ghost: "bg-transparent hover:bg-accent/90",
+            },
+            size: {
+                default: "h-9 px-4 py-2",
+                large: "h-9 px-8",
+                icon: "w-9 h-9",
+            },
+        },
+        defaultVariants: {
+            variant: "default",
+            size: "default",
+        },
+    }
+);
+
+interface ButtonProps
+    extends React.ButtonHTMLAttributes<HTMLButtonElement>,
+        VariantProps<typeof buttonVariants> {
+    asChild?: boolean;
+}
+
+function Button({ asChild, variant, size, ...props }: ButtonProps) {
+    const Comp = asChild ? Slot : "button";
+
+    return (
+        <Comp className={cn(buttonVariants({ variant, size }))} {...props} />
+    );
+}
+
+export { Button };
